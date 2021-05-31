45f884c2a4f5291126d456dde76903e554464ad4
Byron Smith <inadeerbrain@gmail.com>
2021-05-31 16:14:18 -0400

    think working realm

--- app/src/main/java/com/deerbrain/graphdataviewer/Realm/TagDataRealmManager.kt
+++ app/src/main/java/com/deerbrain/graphdataviewer/Realm/TagDataRealmManager.kt
@@ -1,2 +1,12 @@
 package com.deerbrain.graphdataviewer.Realm
 
+import com.deerbrain.graphdataviewer.Charts.TimDataStruct
+
+
+object TagDataRealmManager {
+
+    fun addArrayData(timArray: ArrayList<TimDataStruct>) {
+        var id = (RealmWrapper.realm.where(MapPolys::class.java).max("id")?.toInt() ?: 0) + 1
+    }
+
+}
